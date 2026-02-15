f5422e8dbcf952e0de3b9577510d69d2e9e2c697
umer <syedumerahmedcode@gmail.com>
2026-02-15 17:45:41 +0500

    Feature: Implemented the solution and tested it against a valid test case scenario.

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,29 @@
 public class Solution {
      public int canCompleteCircuit(int[] gas, int[] cost) {
-        
+         /**
+          * Solved using greedy approach
+          */
+         int totalGas = 0;
+         int totalCost = 0;
+         int tank = 0;
+         int startIndex = 0;
+
+         for (int i = 0; i < gas.length; i++) {
+             totalGas += gas[i];
+             totalCost += gas[i];
+             tank += gas[i] - cost[i];
+
+             // If tank is negative, reset the starting index and tank balance
+             if (tank < 0) {
+                 startIndex = i + 1;
+                 tank = 0;
+             }
+         }
+         // If total gas is less tan total cost, return -1
+         if (totalGas < totalCost) {
+             return -1;
+         }
+         return startIndex;
     }
 
 }
